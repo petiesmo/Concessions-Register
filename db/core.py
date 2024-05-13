3bf44210e6a1dda7455cca878105a0d8958b9b2c
petiesmo <66927015+petiesmo@users.noreply.github.com>
2024-05-13 12:13:27 -0600

    Models and database relationships created

--- db/core.py
+++ db/core.py
@@ -1,17 +1,19 @@
-#app/db/core.py
+#db/core.py
 #Core database functions
+from contextlib import contextmanager
 
 from sqlmodel import SQLModel, Session, create_engine
 
 #--- Database
-sqlite_file_name = "database.db"
+sqlite_file_name = "database2.db"
 DATABASE_URL = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(DATABASE_URL, echo=True, connect_args=connect_args)
 
-def create_db_and_tables(engine):
+def create_db_and_tables():
     SQLModel.metadata.create_all(bind=engine)
 
+@contextmanager
 def get_session():
     session = Session(engine)
     yield session
--- models.py
+++ models.py
@@ -1,64 +1,145 @@
-#models/item.py
+#models.py
 #Good template: can be replicated for other item types
+from datetime import datetime
 from typing import List, Optional
+from uuid import uuid4
 
-from pydantic import EmailStr
+from pydantic import EmailStr, BaseModel
+from sqlalchemy import ForeignKey
 from sqlmodel import SQLModel, Field, Relationship, AutoString
 
-#--- GROUP (generic group of Items) Class Options
+#Autostring ref for EmailStr and HttpUrl types: https://github.com/tiangolo/sqlmodel/discussions/730
 #Ref: Teams: https://sqlmodel.tiangolo.com/tutorial/fastapi/teams/
-class GroupBase(SQLModel, table=False):
-    name: str = Field(index=True)
-    param2: str
+#--- CORE MODELS
+class SimpleIDModel(BaseModel):
+    id: int
 
-class Group(GroupBase, table=True):
+
+class UUIDIDModel(BaseModel):
+    id: str = Field(default_factory=uuid4)
+
+
+class TimestampModel(BaseModel):
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: Optional[datetime]
+
+
+class EventTimestamp(BaseModel):
+    occured_at: Optional[datetime]
+
+
+#class Service(SimpleIDModel, TimestampModel, table=True):
+#    name: str = Field(nullable=False)
+#    description: str = Field(nullable=False)
+
+
+#--- CUSTOMER class options ---
+class CustomerBase(SQLModel, table=False):
+    name: str = Field(index=True, nullable=False)
+    staff: bool = Field(index=True, nullable=False, default=False)
+    acct_balance: float = Field(default=0.00, nullable=False)
+
+
+class Customer(CustomerBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    items: List['Item'] = Relationship(back_populates='group')
+    transactions: List['Transaction'] = Relationship(back_populates='customer')
 
-class GroupCreate(GroupBase):
+
+class CustomerCreate(CustomerBase):
     pass
 
-class GroupRead(GroupBase):
-    id: str
 
-class GroupShort(GroupBase):
-    group_id: str
+class CustomerRead(CustomerBase):
+    id: int
+
+
+class CustomerShort(CustomerBase):
     name: str
 
-class GroupUpdate(GroupBase):
-    group_id: str
-    item_id: str
-    name: Optional[str] = None
-    param2: Optional[str] = None
 
+#Update Ref: https://sqlmodel.tiangolo.com/tutorial/fastapi/update/
+class CustomerUpdate(BaseModel):
+    name: Optional[str] = None
+    staff: Optional[bool] = None
+    acct_balance: Optional[float] = None
+
+
+
+#--- IMPORTANT Linkage for a Many-to-Many relationship (in this case, between Transactions & Items)
+#Ref: https://sqlmodel.tiangolo.com/tutorial/many-to-many/create-models-with-link/
+class TxItemLink(SQLModel, table=True):
+    tx_id: int | None = Field(default=None, foreign_key="transaction.id", primary_key=True)
+    item_id: int | None = Field(default=None, foreign_key="item.id", primary_key=True)
+
+
+
+#--- TX = Transaction (generic) Class Options 
+class TxBase(SQLModel, TimestampModel, table=False):
+    customer_id: Optional[int] = Field(default=None, foreign_key='customer.id')
+    discount: Optional[float] = Field(default=0.00)
+    total: float
+
+
+class Transaction(TxBase, table=True, extend_existing=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    customer: Customer = Relationship(back_populates='transactions')
+    items: List['Item'] = Relationship(link_model=TxItemLink)
     
-#--- ITEM (generic) Class Options 
+
+class TxCreate(TxBase):
+    pass
+
+
+class TxRead(TxBase):
+    id: str
+
+
+class TxShort(TxBase):
+    id: str
+    name: str
+
+
+'''For now, Tx will only be created or deleted
+class TxUpdate(SQLModel):
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    customer_id: Optional[int]
+    items: Optional[List]
+    total: Optional[float]
+'''
+
+
+
+#--- Items Class Options ---
 class ItemBase(SQLModel, table=False):
-    name: str = Field(index=True)
-    email: EmailStr = Field(unique=True, index=True, sa_type=AutoString)
-    phone: str = Field()
-    group_id: Optional[int] = Field(default=None, foreign_key='group.id')
+    name: str = Field(nullable=False)
+    description: Optional[str]
+    price: float = Field(default=0.00, nullable=False)
+    qty: float = 0
 #Autostring ref: https://github.com/tiangolo/sqlmodel/discussions/730
 
 class Item(ItemBase, table=True, extend_existing=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    group: Optional[Group] = Relationship(back_populates='items')
-    param1: Optional[str] = ''
+
 
 class ItemCreate(ItemBase):
     pass
 
+
 class ItemRead(ItemBase):
-    id: str
+    id: int
+    name: str
+    price: float
+    qty: float
+
 
 class ItemShort(ItemBase):
-    id: str
+    id: int
     name: str
-    
-class ItemUpdate(ItemBase):
-    item_id: str
-    name: Optional[str] = None
-    email: Optional[str] = None
-    phone: Optional[str] = None
-    group_id: Optional[int] = None
+    price: float
 
+
+class ItemUpdate(SQLModel):
+    name: Optional[str]
+    description: Optional[str]
+    price: Optional[float]
+    qty: Optional[float]    
